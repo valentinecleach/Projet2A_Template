cb5ea5ed03697e9bf2ad06e96a1bb27ce29db01a
Clément Yvernault <clement.yvernault-collet@eleve.ensai.fr>
2024-09-18 17:51:00 +0200

    API

--- src/Interface/API.py
+++ src/Interface/API.py
@@ -28,3 +28,6 @@ def run_app(movie_service: MovieService):
     uvicorn.run(app, port=8000, host="localhost")
 
 # la clé API : fb0e5ded3d79bc5e571538030f7e5af8
+# le token : eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYjBlNWRlZDNkNzliYzVlNTcxNTM4MDMwZjdlNWFmOCIsIm5iZiI6MTcyNjY2ODg3MS44OTQ2MjMsInN1YiI6IjY2ZTBhYmMyOWM3MzUzMmRkYmFhYWY0NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.cZ0F3epj5FCX4MRlrqSAIdzErJP98tU9ZlfNHkrfUw0
+
+
