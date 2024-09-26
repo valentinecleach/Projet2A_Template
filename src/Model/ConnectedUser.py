d11c7a712012bb3cbfc987ee61302804ce91f361
Clément Yvernault <clement.yvernault-collet@eleve.ensai.fr>
2024-09-26 23:03:25 +0200

    classe utilisateur

---
+++ src/Model/ConnectedUser.py
@@ -0,0 +1,37 @@
+from User import User
+from Movie import Movie
+
+class ConnectedUser(User):
+    def __init__(self, name: str, pseudo : str, email: str, password: str, date_of_birth: str, phone_number: str = None):
+        super().__init__(ip_address=None)  # Pas d'adresse IP pour l'utilisateur connecté
+        self.name = name
+        self.pseudo = pseudo
+        self.email = email
+        self.password = password  # hacher ce mot de passe
+        self.date_of_birth = date_of_birth
+        self.phone_number = phone_number
+        self.film_collection = []  # Liste des films ajoutés par l'utilisateur
+        self.follow_list = []  # Liste des utilisateurs suivis
+
+    def add_film(self, film : Movie):
+        """Ajoute un film à la collection de l'utilisateur."""
+        self.film_collection.append(film)
+
+    def follow(self, utilisateur):
+        """Ajoute un utilisateur à la liste des éclaireurs."""
+
+    def unfollow(self, utilisateur):
+        """Supprime un utilisateur de la liste des éclaireurs."""
+
+    def rate_film(self, film, rating: int):
+        """Attribue une note à un film."""
+        
+    def add_comment(self, film, comment: str):
+        """Ajoute un commentaire à un film."""
+
+    def log_out(self):
+        """Déconnexion de l'utilisateur."""
+        
+    def delete_account(self):
+        """Suppression du compte de l'utilisateur."""
+       
