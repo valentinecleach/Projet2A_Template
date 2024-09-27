93fd1c10d7f24adb25e686a7c5ab353e9cbf2c8c
valentinecleach <valentine.cleach@gmail.com>
2024-09-27 15:41:31 +0200

    movie

--- src/DAO/Movie_dao.py
+++ src/DAO/Movie_dao.py
@@ -1,10 +1,12 @@
-from typing import List, Optional         
+from typing import List, Optional
+
 from DAO.db_connection import DBConnection
 from Model.Movie import Movie
 
+
 class Movie_dao :
     # structure prise du TP
-    def find_Movie_by_id(self, id:int) -> Optional[AbstractAttack]:
+    def find_Movie_by_id(self, id:int) -> Movie :
         with DBConnection().connection as connection:
             with connection.cursor() as cursor:
                 try:
@@ -12,14 +14,14 @@ class Movie_dao :
                         "SELECT *                                  "
                         "  FROM tp.attack a                     "
                         f"  WHERE a.id_attack = {id}"
-                    )       
+                    )
                     res = cursor.fetchone()
                 except Exception as e:
                     print(f" Erreur : {e}")
                     return None
                 return res
 
-        
+
 """
 tout ceci est inclu si on utilisedes with
 conn.commit()
