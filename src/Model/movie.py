36ad20919f3d3d0a2b10d501856262c80ac621fa
valentinecleach <valentine.cleach@gmail.com>
2024-10-03 22:12:57 +0200

    jsp ce que c'est mon doc etait pas sauvegardé...

--- src/Model/movie.py
+++ src/Model/movie.py
@@ -1,6 +1,8 @@
-from src.Model.Genre import Genre
 from pydantic import BaseModel
+
+from src.Model.Genre import Genre
 from src.Service.MovieMakerService import MovieMakerService as MMS
+
 #from src.Model.Rating import Rating
 
 
