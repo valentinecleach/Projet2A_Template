888fe89b68d017c853c600be92c4d75b15d8eddf
valentinecleach <valentine.cleach@gmail.com>
2024-10-03 20:08:02 +0200

    val fait nimporte quoi mais jessaie attendez la fin du repas

--- src/Model/Genre.py
+++ src/Model/Genre.py
@@ -1,7 +1,21 @@
 class Genre:
+    """
+    Class representing a genre with an id and a name.
+
+    Parameters
+    ----------
+    id : int
+        An id that allows easy recognition of the genre
+    name : str
+        The genres name
+
+    Examples
+    --------
+    print(Genre(genre_data={'id': 123, 'name': "Musical Comedy"}))
+
+    """
     def __init__(self, genre_data: dict):
-        """
-        Class representing a genre with an id and a name.
+        """Constructor
 
         Parameters:
         -----------
@@ -15,6 +29,12 @@ class Genre:
         return f"Genre(id={self.id}, name='{self.name}')"
 
 # Unrelated code to show sys.path to understand error with classes ipmports.
+
+# code to prepare the doctests
+print(Genre(genre_data={'id': 123, 'name': "Musical Comedy"}))
+
+
+
 """
 import sys
 
