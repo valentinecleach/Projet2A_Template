ff2634ae936ed1df33a7c651d13b8ac976831196
valentinecleach <valentine.cleach@gmail.com>
2024-10-10 22:26:32 +0200

    feat: MovieDao update )

--- src/DAO/Movie_dao.py
+++ src/DAO/Movie_dao.py
@@ -26,33 +26,53 @@ class Movie_dao:
             with connextion.cursor() as cursor:
                 # SQL resquest
                 cursor.execute("""
-                    INSERT INTO movie (
-                               )
-                    VALUES (
-                               )
-                               """,
-                               valeurs)
+                    INSERT INTO new_movie (adult, genre_ids, id, original_title, overview, 
+                                        popularity, release_date, title, vote_average, 
+                                        vote_count)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                    """,
+                               (new.movie.adult, new.movie.genre.id, new.movie.original_title, new.movie.overview,
+                               new.movie.popularity, new.movie.release_date, new.movie.title, new.movie.vote_average,
+                               new.movie.vote))
+                               # tagline? status?)
             # Fetching the result
             res = cursor.fetchone()
             
         # if we have a result
         if res:
-            what_we_returne = 
+            pass
+            # what_we_returne = 
             print("Insersion successful : Movie added.")
         except Exception as e:
             print("Insersion error : ", str(e))
-        return what_we_return
+        return None #what_we_return
 
+    def update(self, movie : Movie):
+        try:
+            with self.db_connection.connection.cursor() as cursor:
+                cursor.execute("""
+                    UPDATE movie
+                    SET adult = %s, genre.id = %s , original_title = %s, overview = %s,
+                               popularity = %s, release_date = %s, title = %s, vote_average = %s,
+                               vote = %s
+                    WHERE id_movie = %s;
+                """, (movie.adult, movie.genre.id, movie.original_title, movie.overview,
+                               movie.popularity, movie.release_date, movie.title, movie.vote_average,
+                               movie.vote))
+                self.db_connection.connection.commit()
+                print("Update successful : movie updated.")
+        except Exception as e:
+            print("Update error : ", str(e))
 
-    # structure prise du TP
+    # structure prise du TP --> mauvaise classe
     def find_Movie_by_id(self, id: int) -> Movie:
         with DBConnection().connection as connection:
             with connection.cursor() as cursor:
                 try:
                     cursor.execute(
                         "SELECT *                                  "
-                        "  FROM tp.attack a                     "
-                        f"  WHERE a.id_attack = {id}"
+                        "  FROM                      "
+                        f"  WHERE id_movie = {id}"
                     )
                     res = cursor.fetchone()
                 except Exception as e:
@@ -74,4 +94,31 @@ cursor.fetchmany(size)
 
 Pour modifier
 cursor.execute()
+"""
+
+# https://api.themoviedb.org/3/search/movie?query=bullet%20train&include_adult=false&language=en-US&page=1
+"""
+{
+  "page": 1,
+  "results": [
+    {
+      "adult": false,
+      "backdrop_path": "/y2Ca1neKke2mGPMaHzlCNDVZqsK.jpg",
+      "genre_ids": [
+        28,
+        35,
+        53
+      ],
+      "id": 718930,
+      "original_language": "en",
+      "original_title": "Bullet Train",
+      "overview": "Unlucky assassin Ladybug is determined to do his job peacefully after one too many gigs gone off the rails. Fate, however, may have other plans, as Ladybug's latest mission puts him on a collision course with lethal adversaries from around the globe—all with connected, yet conflicting, objectives—on the world's fastest train.",
+      "popularity": 63.204,
+      "poster_path": "/tVxDe01Zy3kZqaZRNiXFGDICdZk.jpg",
+      "release_date": "2022-08-03",
+      "title": "Bullet Train",
+      "video": false,
+      "vote_average": 7.456,
+      "vote_count": 6075
+    }
 """
