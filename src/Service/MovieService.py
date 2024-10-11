a4a7ff2b4e9e57f011f4ba702185e69f9e95324b
valentinecleach <valentine.cleach@gmail.com>
2024-10-11 14:44:45 +0200

    fix: titles of model

--- src/Service/MovieService.py
+++ src/Service/MovieService.py
@@ -7,6 +7,27 @@ class MovieService:
     def __init__(self, movie_db: None):
         self.movie_db = movie_db
 
-    def get_by_id(self, movie_id: int) -> Movie:
+    def find_by_id(self, movie_id: int) -> Movie:
+        """Find movie by id"""
         return Movie(id=1, original_title="A Clockwork Orange")
         # return self.movie_db.get_by_id(movie_id)
+
+    def find_by_title(self, movie_title:str) -> Movie:
+        """Find movie by title"""
+        pass
+
+    def view_comments(self, movie:Movie) -> ... :
+        """View the comments of a movie"""
+        pass
+
+    def filter_by_genre(self, genre: Genre) -> ... :  
+        """Filter movies by their genre"""
+        pass
+
+    def filter_by_popularity(self) -> list[Movie] :
+        """Filters the movie by the popularity"""
+        pass
+
+    def find_movie_maker(self, maker:str) -> MovieMaker:
+        """Finds a movie maker"""
+        pass
