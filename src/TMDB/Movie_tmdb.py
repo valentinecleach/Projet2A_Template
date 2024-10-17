f03a8cbaf15a63d5e4a64da61101593e81a1c783
valentinecleach <valentine.cleach@gmail.com>
2024-10-17 23:34:00 +0200

    feat : filter popularity MovieTMDB

--- src/TMDB/Movie_tmdb.py
+++ src/TMDB/Movie_tmdb.py
@@ -135,13 +135,50 @@ class MovieTMDB:
         """
         pass
 
-    def filter_by_popularity(self) -> Movie:
-        """Filters by popularity
+    def filter_by_popularity(self) -> list(Movie):
+        """Filters by popularity all movies of the TMDB database and returns
 
         Parameters
         ----------
+
+        Returns
+        --------
+        topmovies : list(Movie)
+            The top movies from page 1.
         """
-        pass
+        try:
+            url = f"{self.base_url}/movie/popular?api_key={self.api_key}&language=en-US&page=1"
+            # url = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1"
+            headers = {"accept": "application/json", "Authorization": self.api_key}
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()  # Raises an exception for HTTP error codes.
+            data = response.json()
+
+            topmovies = []
+            for movie in data["results"]:
+                topmovies.append(
+                    Movie(
+                        # add things
+                        """
+                        "adult",
+                        "genre_ids",
+                        "id",
+                        "original_language",
+                        "original_title",
+                        "overview",
+                        "popularity",
+                        "release_date",
+                        "title",
+                        "video",
+                        "vote_average",
+                        "vote_count"
+                        """
+                    )
+                )
+            return topmovies
+        except requests.exceptions.RequestException as e:
+            print("Error while fetching the movies: ", str(e))
+            return None
 
     def find_movie_maker(self, id_movie) -> MovieMaker:
         """Finds the movie makers of a movie
@@ -153,7 +190,7 @@ class MovieTMDB:
 
         Returns
         --------
-        list(MovieMaker) | None
+        movie_makers : list(MovieMaker) | None
         """
         try:
             url = f"{self.base_url}/movie/{id_movie}/credits?api_key={self.api_key}&language=en-US"
@@ -180,6 +217,8 @@ class MovieTMDB:
                             deathday=moviemakers["deathday"],
                         )
                     )
+
+                return movie_makers
             else:
                 print(f"No movie makers found with the info given : {id_movie}.")
                 return None
@@ -190,32 +229,3 @@ class MovieTMDB:
 
 # https://developer.themoviedb.org/reference/movie-credits
 # get https://api.themoviedb.org/3/movie/{movie_id}/credits
-"""
-{
-  "id": 21661,
-  "cast": [
-    {
-      "adult": false,
-      "gender": 2,
-      "id": 7431,
-      "known_for_department": "Acting",
-      "name": "Kevin Zegers",
-      "original_name": "Kevin Zegers",
-      "popularity": 18.321,
-      "profile_path": "/zRjasG4NXxKbe6e1c4enivAvfcH.jpg",
-      "cast_id": 3,
-      "character": "Josh Framm",
-      "credit_id": "52fe4423c3a368484e0118c3",
-      "order": 0
-    },
-
-
-    import requests
-
-url = "https://api.themoviedb.org/3/movie/21661/credits?language=en-US"
-
-
-response = requests.get(url, headers=headers)
-
-print(response.text)
-"""
