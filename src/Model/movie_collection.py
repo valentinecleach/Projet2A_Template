ae39f3defa3dad9e6ce2d44bfad3d683097ce564
valentinecleach <valentine.cleach@gmail.com>
2024-10-21 16:33:05 +0200

    fix: movie collection __init__

--- src/Model/movie_collection.py
+++ src/Model/movie_collection.py
@@ -21,10 +21,6 @@ class MovieCollection:
     """
 
     def __init__(self, movie_collection_data: Dict):
-        self.name = movie_collection_data.get("name")
-        self.id = movie_collection_data.get("id")
-
-    def __init__(self, name: str, id: int):
         """Constructor
 
         Parameters:
@@ -32,8 +28,8 @@ class MovieCollection:
         MovieCollection_data : dict
             A dictionary containing the collection of the movie data.
         """
-        self.name = name
-        self.id = id
+        self.name = movie_collection_data.get("name")
+        self.id = movie_collection_data.get("id")
 
     def __repr__(self):
         return f" 'Movie Collection : {self.name}' "
