65476b33df26e891f36e03bd1877219b54c511d4
valentinecleach <valentine.cleach@gmail.com>
2024-10-21 15:35:09 +0200

    fix

--- src/Model/Genre.py
+++ src/Model/Genre.py
@@ -14,6 +14,7 @@ class Genre:
     print(Genre(genre_data={'id': 123, 'name': "Musical Comedy"}))
 
     """
+
     def __init__(self, genre_data: dict):
         """Constructor
 
@@ -22,18 +23,15 @@ class Genre:
         genre_data : dict
             A dictionary containing the genre data (id and name).
         """
-        self.id = genre_data.get('id')
-        self.name = genre_data.get('name')
+        self.id = genre_data.get("id")
+        self.name = genre_data.get("name")
 
     def __repr__(self):
-       return f"'Genre : {self.name}'"
+        return f"'Genre : {self.name}'"
+
 
 # Unrelated code to show sys.path to understand error with classes ipmports.
 
 # code to prepare the doctests
-genre_action = {
-			"id": 28,
-			"name": "Action"
-		}
+genre_action = {"id": 28, "name": "Action"}
 print(Genre(genre_data=genre_action))
-
