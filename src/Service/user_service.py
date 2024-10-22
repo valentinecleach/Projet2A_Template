3b07bdd15505a8ca98dd9dea1a14bd9a0f37d5d7
valentinecleach <valentine.cleach@gmail.com>
2024-10-22 10:46:29 +0200

    feat: ajout de check_password_strenght dans le sign up

--- src/Service/user_service.py
+++ src/Service/user_service.py
@@ -13,6 +13,9 @@ class UserService:
         if len(username) < 5:
             raise ValueError("Le nom d'utilisateur doit comporter au moins 5 caractères.")
 
+        # Validation du mot de passe 
+        check_password_strenght(password)
+        
         # Création des parties de sel
         start_user = username[:3]  # Les trois premiers caractères du nom d'utilisateur
         end_user = username[3:]  # Les caractères restants du nom d'utilisateur
