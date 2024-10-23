76370f2bbae317d92c48cc335d2ae9ff5f2f2882
Clément Yvernault <clement.yvernault-collet@eleve.ensai.fr>
2024-10-23 11:16:57 +0200

    ok

--- src/Webservice/movie_controller.py
+++ src/Webservice/movie_controller.py
@@ -17,3 +17,16 @@ def get_movie_by_id(tmdb_id: int) -> Movie:
         ) from FileNotFoundError
     except Exception:
         raise HTTPException(status_code=400, detail="Invalid request") from Exception
+
+@movie_router.get("/{title}",response_model= Movie, status_code=status.HTTP_200_OK)
+def get_movie_by_title(title: int) -> Movie:
+    try:
+        my_movie = MovieService().get_movie_by_name(name)
+        return my_movie
+    except FileNotFoundError:
+        raise HTTPException(
+            status_code=404,
+            detail="Movie with title [{}] not found".format(name),
+        ) from FileNotFoundError
+    except Exception:
+        raise HTTPException(status_code=400, detail="Invalid request") from Exception
