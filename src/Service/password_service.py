722ee188d26a390b3aa149d8841d64ff8589b7b4
Gaspard.Poirier <gaspard.poirier@eleve.ensai.fr>
2024-10-23 10:49:27 +0200

    poupou18

--- src/Service/password_service.py
+++ src/Service/password_service.py
@@ -4,30 +4,33 @@ from typing import Optional
 
 # DAO
 from src.DAO.user_dao import UserDao
+from src.Model.connected_user import ConnectedUser
 
 # Model
 from src.Model.user import User
-from src.Model.connected_user import ConnectedUser
 
-#hash password and salt password in user_service
+# hash password and salt password in user_service
+
 
 def hash_password(password: str, salt: Optional[str] = None) -> str:
-    """ Hashes the password
-    """
-    salted_password = f"{salt[0]}{password}{salt[1]}".encode('utf-8')
+    """Hashes the password"""
+    salted_password = f"{salt[0]}{password}{salt[1]}".encode("utf-8")
     hashed_password = bcrypt.hashpw(salted_password, bcrypt.gensalt())
     return hashed_password
 
-def create_salt(username : str) -> list[str]:
-    """ Creates a salt for the password to be hashed
-    """
+
+def create_salt(username: str) -> list[str]:
+    """Creates a salt for the password to be hashed"""
     # Création des parties de sel
     start_salt = username[:3]  # Les trois premiers caractères du nom d'utilisateur
-    end_salt = username[3:] + secrets.token_hex(16) # Les caractères restants du nom d'utilisateur et une clef secrete.   
+    end_salt = username[3:] + secrets.token_hex(
+        16
+    )  # Les caractères restants du nom d'utilisateur et une clef secrete.
     salt = [start_user, end_user]
 
+
 def check_password_strenght(password: str):
-    """ Checks a passwords strenght. 
+    """Checks a passwords strenght.
     If the password is not strong enough (too short, not enough characters etc), an exception wil be raised
 
     Parameters
@@ -40,11 +43,11 @@ def check_password_strenght(password: str):
     length = len(password)
     number = False
     for i in length:
-        if isinstance(password[i-1], int): 
+        if isinstance(password[i - 1], int):
             number = True
-    if number = False:
-        raise Exception('Password must contain at least one number')
-    
+    if number == False:
+        raise Exception("Password must contain at least one number")
+
 
 def validate_username_password(username: str, password: str, user_dao: UserDao) -> User:
     connected_user: Optional[User] = user_dao.get_user_by_name(username=username)
