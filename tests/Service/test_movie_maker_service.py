b664096620c68614084d46bdf48f22077e3a873d
valentinecleach <valentine.cleach@gmail.com>
2024-10-23 16:46:16 +0200

    feat : tets_movie_maker_service

---
+++ tests/Service/test_movie_maker_service.py
@@ -0,0 +1,27 @@
+import pytest
+
+from src.Model.movie_maker import MovieMaker
+from src.Service.movie_maker_service import MovieMakerService
+
+@pytest.fixture
+def movie_maker_data():
+    """Fixture pour fournir des données de collection de films."""
+    return {
+        
+    }
+
+def test_get_movie_maker_by_name_listMM(self, name: str, test):
+    pass
+
+def test_get_movie_maker_by_name_None(self, name: str, test):
+    pass
+
+def test_get_movie_maker_by_id_MovieMaker(self, id : int, test):  
+    pass
+    
+def test_get_movie_maker_by_id_None(self, id : int, test):
+    pass
+    
+        
+
+def
