8b7e6c1fd8156b7db219d2acdd66281875420a80
valentinecleach <valentine.cleach@gmail.com>
2024-11-10 18:02:57 +0100

    fix: tests: mock_db_connection + initialization

--- tests/DAO/test_db.py
+++ tests/DAO/test_db.py
@@ -3,37 +3,72 @@
 
 import os
 from unittest.mock import MagicMock, patch
+from unittest import mock
 
 import pytest
-
+# python -m pip install mock
+# python -m pip install pytest-mock
 from src.DAO.db_connection import DBConnection
 
 
-@pytest.fixture
+
 def mock_db_connection(mocker):
     """Fixture pour simuler la connexion à la base de données."""
+    # Mock dotenv.load_dotenv pour ne pas lire les infos du .env
+    mocker.patch("dotenv.load_dotenv", return_value=None)
+    
     # Configuration de l'environnement de test pour utiliser le schéma projet_info_test
-    os.environ["host"] = "localhost"
-    os.environ["port"] = "5432"
-    os.environ["dbname"] = "test_db"
-    os.environ["user"] = "test_user"
-    os.environ["password"] = "test_password"
-    os.environ["pro"] = "projet_info_test"
-    os.environ["test"] = "projet_info_test"
+    mocker.patch.dict(os.environ, {
+        "host": "localhost",
+        "port": "5432",
+        "dbname": "test_db",
+        "user": "test_user",
+        "password": "test_password",
+        "schema": "projet_info_test"
+    }, clear=True) # clear pour bien effacer les variables précédamment dans le test
 
-    # Simule une connexion de base de données
-    mock_connect = mocker.patch(
+    mocker.patch(
         "src.DAO.db_connection.psycopg2.connect", return_value=MagicMock()
     )
-    mock_connection = mock_connect.return_value
-
+    
     # Retourne une instance de DBConnection pour les tests
-    return DBConnection(test=True), mock_connection
+    return DBConnection()
 
 
-def test_initialization(mock_db_connection):
-    db_connection, mock_connection = mock_db_connection()
-    assert db_connection.connection == mock_connection
+def test_initialization_no_config(mocker):
+    # GIVEN / WHEN
+    db_connection = mock_db_connection(mocker)
+
+    # THEN
+    assert db_connection.host == "localhost" # "sgbd-eleves.domensai.ecole" != "localhost"
+    assert db_connection.port == "5432"
+    assert db_connection.database == "test_db"
+    assert db_connection.user == "test_user"
+    assert db_connection.password == "test_password"
+    assert db_connection.schema == "projet_info_test"
+    
+
+def test_initialization_config():
+    # GIVEN
+    config = {
+        "host": "localhost",
+        "port": "5432",
+        "database": "test_db",
+        "user": "test_user",
+        "password": "test_password",
+        "schema": "projet_info_test"
+    }
+
+    # WHEN
+    db = DBConnection(config=config)
+
+    # THEN
+    assert db.host == "localhost"
+    assert db.port == "5432"
+    assert db.database == "test_db"
+    assert db.user == "test_user"
+    assert db.password == "test_password"
+    assert db.schema == "projet_info_test"
 
 
 def test_set_search_path(mock_db_connection):
@@ -48,12 +83,13 @@ def test_set_search_path(mock_db_connection):
 def test_create_tables(mock_db_connection):
     db_connection, mock_connection = mock_db_connection
     cursor_mock = mock_connection.cursor.return_value.__enter__.return_value
+   
     db_connection.create_tables()
 
     calls = [
-        unittest.mock.call(
-            "CREATE TABLE IF NOT EXISTS MovieMaker (id_movie_maker SERIAL PRIMARY KEY, adult BOOLEAN NOT NULL DEFAULT FALSE, name VARCHAR(255) NOT NULL, gender INTEGER NOT NULL, biography TEXT NOT NULL, birthday DATE, place_of_birth VARCHAR(255), deathday DATE, known_for_department VARCHAR(255), popularity FLOAT NOT NULL, known_for JSONB);"
-        ),
+       unittest.mock.call(
+          "CREATE TABLE IF NOT EXISTS MovieMaker (id_movie_maker SERIAL PRIMARY KEY, adult BOOLEAN NOT NULL DEFAULT FALSE, name VARCHAR(255) NOT NULL, gender INTEGER NOT NULL, biography TEXT NOT NULL, birthday DATE, place_of_birth VARCHAR(255), deathday DATE, known_for_department VARCHAR(255), popularity FLOAT NOT NULL, known_for JSONB);"
+       ),
         unittest.mock.call(
             "CREATE TABLE IF NOT EXISTS users (id_user SERIAL PRIMARY KEY, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, username VARCHAR(255) UNIQUE NOT NULL, hashed_password VARCHAR(255) NOT NULL, email VARCHAR(255) UNIQUE NOT NULL);"
         ),
@@ -67,16 +103,16 @@ def test_insert(mock_db_connection):
     db_connection, mock_connection = mock_db_connection
     test_table = "users"
     test_values = ("John", "Doe", "john_doe", "hashed_password", "john@example.com")
-
     cursor_mock = mock_connection.cursor.return_value.__enter__.return_value
     cursor_mock.fetchall.return_value = [
         {"column_name": "first_name"},
         {"column_name": "last_name"},
         {"column_name": "username"},
         {"column_name": "hashed_password"},
+
         {"column_name": "email"},
     ]
-
+    
     db_connection.insert(test_table, test_values)
 
     cursor_mock.execute.assert_called_once()
