349f6456c942d1081338113b62330f3d8c1a335f
valentinecleach <valentine.cleach@gmail.com>
2024-11-15 17:00:06 +0100

    fix: tests service

--- src/Service/password_service.py
+++ src/Service/password_service.py
@@ -1,6 +1,6 @@
 import hashlib
 import secrets
-from typing import Optional
+from typing import Optional, List
 
 import bcrypt
 
@@ -14,17 +14,20 @@ import bcrypt
 # hash password and salt password in user_service
 
 
-def hash_password(password: str, salt: Optional[str] = None) -> str:
-    """Hashes the password"""
-    salted_password = f"{salt[0]}{password}{salt[1]}".encode("utf-8")
-    hashed_password = hashlib.sha256(salted_password).hexdigest() 
+def hash_password(password: str, salt: Optional[List[str]] = None) -> str:
+    """Hashes the password. The salt is optional"""
+    if salt is None:
+        hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+    else:
+        salted_password = f"{salt[0]}{password}{salt[1]}".encode("utf-8")
+        hashed_password = hashlib.sha256(salted_password).hexdigest() 
     return hashed_password
 
 
-def create_salt(username: str, password_token : Optional[str] = None) -> list[str]:
+def create_salt(username: str, ) -> list[str]:
     """Creates a salt for the password to be hashed"""
     # Création des parties de sel
-    password_token = password_token if password_token else secrets.token_hex(16)
+    password_token = secrets.token_hex(16)
     start_salt = username[:3]  # Les trois premiers caractères du nom d'utilisateur
     end_salt = (
         username[3:] + password_token
@@ -55,6 +58,7 @@ def check_password_strenght(password: str):
             number = True
     if number is False:
         raise ValueError("Password must contain at least one number")
+
 
 
 # def validate_username_password(username: str, password: str, user_dao: UserDao) -> User:
--- tests/Service/test_password_service.py
+++ tests/Service/test_password_service.py
@@ -1,41 +1,37 @@
+# pip install bcrypt
 import bcrypt
 import pytest
+import hashlib
 
-from src.Service.password_service import (check_password_strenght, create_salt,
-                                          create_token, hash_password)
+from src.Service.password_service import (check_password_strenght, create_salt, hash_password)
 
+#python -m pytest tests/Service/test_password_service.py
 
 # Test du hashage de mot de passe
-def test_hash_password():
-    password = "strongpassword123"
-    username = "testuser"
-    token = create_token(username)
-    salt = create_salt(username, token)
+def test_hash_password_no_salt():
+    password = "NoCode"
+    hashed_password = hash_password(password)
+    assert hashed_password == "9da90a168c5026a3291f7612de2caebc7e36ae4c630416fc3332a41a1c630fee"
 
+def test_hash_password_with_salt():
+    password = "NoCode"
+    salt = ["tes", "tuser", "2"]
     hashed_password = hash_password(password, salt)
-
-    # Vérification que le mot de passe a bien été haché
-    assert bcrypt.checkpw(f"{salt[0]}{password}{salt[1]}".encode('utf-8'), hashed_password) is True
+    assert hashed_password == "d9812273fc89e21a4840370fad863191e476a08d4caa36dcd33a82f4fc4b9cdb"
 
 
-# Test de la création de sel
-def test_create_salt():
+# Test de la création de sel sans token
+def test_create_salts():
+    # GIVEN
     username = "testuser"
-    token = create_token(username)
-    salt = create_salt(username, token)
 
-    assert len(salt) == 2  # Le sel doit être composé de 2 parties
+    # When
+    salt = create_salt(username)
+
+    # THEN
+    assert len(salt) == 3  # Le sel doit être composé de 2 parties
     assert salt[0] == "tes"  # Les trois premiers caractères du username
-    assert salt[1] == "tuser" + token  # Les caractères restants du username + le token
-
-
-# Test de la création de token
-def test_create_token():
-    username = "testuser"
-    token = create_token(username)
-
-    assert isinstance(token, str)
-    assert len(token) == 32  # Un token hexadécimal de 16 bytes génère une chaîne de 32 caractères
+    assert salt[1] == "tuser"+salt[2]   # Les caractères restants du username + le token
 
 
 # Test de la vérification de la force du mot de passe
