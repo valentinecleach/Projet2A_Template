02901ed6ee2926646f592a00b5508187abce0884
Gaspard.Poirier <gaspard.poirier@eleve.ensai.fr>
2024-11-20 16:10:42 +0100

    poupou45

--- src/Service/movie_service.py
+++ src/Service/movie_service.py
@@ -66,29 +66,6 @@ class MovieService:
                 list_movies.append(movie)
         return list_movies
 
-    #################################################################################################
-
-    def find_by_id(self, movie_id: int) -> Movie:
-        """Find movie by id"""
-        return Movie(id=1, original_title="A Clockwork Orange")
-        # return self.movie_db.get_by_id(movie_id)
-
-    def find_by_title(self, movie_title: str) -> Movie:
-        """Find movie by title"""
-        pass
-
-    def view_comments(self, movie: Movie) -> ...:
-        """View the comments of a movie"""
-        pass
-
-    #    def filter_by_genre(self, genre: Genre) -> ...:
-    #       """Filter movies by their genre"""
-    #        pass
-
-    def filter_by_popularity(self) -> list[Movie]:
-        """Filters the movie by the popularity"""
-        pass
-
 
 # db_connection = DBConnector()
 # my_object = MovieService(db_connection)
