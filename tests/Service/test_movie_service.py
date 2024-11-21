61e18432ea4c5c9eb6149e12822e9dafc5d42f59
valentinecleach <valentine.cleach@gmail.com>
2024-11-21 11:55:24 +0100

    feat: create test_create_movies in test_movie_service

--- tests/Service/test_movie_service.py
+++ tests/Service/test_movie_service.py
@@ -195,5 +195,38 @@ def test_get_movie_by_title_not_found():
     assert result is None
 
 # ------ CREATE MOVIES -----------
+def test_create_movies():
+    # GIVEN
+    movieservice = MovieService(MockDBConnection)
 
-# ------ GET MOVIE BY ID -----------
+    movie_data = [{"id": 19995}]
+
+    # WHEN / THEN
+    list_movies = movieservice.create_movies(movie_data)[0]
+    
+    movie = Movie(
+        id_movie=19995,
+        title="Avatar",
+        belongs_to_collection=[MovieCollection(id=87096, name="Avatar Collection")],
+        budget=237000000,
+        genres=[
+            Genre(id=28, name="Action"),
+            Genre(id=12, name="Adventure"),
+            Genre(id=14, name="Fantasy"),
+            Genre(id=878, name="Science Fiction"),
+        ],
+        origin_country=["US"],
+        original_language="en",
+        original_title="Avatar",
+        overview="In the 22nd century, a paraplegic Marine is dispatched to the moon Pandora on a unique mission, but becomes torn between following orders and protecting an alien civilization.",
+        popularity=99.959,
+        release_date=datetime.date(2009, 12, 15),
+        revenue=2923706026,
+        runtime=162,
+        vote_average=7.582,
+        vote_count=31385,
+        adult=False,
+    )
+
+    assert list_movies.title == "Avatar"
+
