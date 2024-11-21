82a73a20caba70e89766a07d660471ad708a5a42
valentinecleach <valentine.cleach@gmail.com>
2024-11-21 12:46:29 +0100

    fix: code + leger dans les tests

--- tests/Service/test_movie_maker_service.py
+++ tests/Service/test_movie_maker_service.py
@@ -5,15 +5,6 @@ from src.Service.movie_maker_service import MovieMakerService
 from tests.DAO.test_db import MockDBConnection
 from src.DAO.db_connection import DBConnector
 
-#@pytest.fixture
-#def movie_maker_data():
-#    """Fixture pour fournir des données de collection de films."""
-#    return {
-#        movie_maker_dao = 
-#        movie_maker_tmdb = 
-        
-#    }
-
 def test_get_movie_maker_by_name_found_DB():
     # GIVEN
     moviemakerservice = MovieMakerService(DBConnector)
@@ -45,14 +36,3 @@ def test_get_movie_maker_by_name_not_found():
 
     # THEN
     assert result is None
-
-    
-    """    movie_maker= MovieMaker(id_movie = 1392137,
-                            adult = False,
-                            biography = "Sarah Margaret Qualley (born October 23, 1994) is an American actress. The daughter of actress Andie MacDowell, she trained as a ballerina in her youth and briefly pursued a career in modeling. She made her acting debut with a minor role in the 2013 drama film Palo Alto and gained recognition for playing a troubled teenager in the HBO drama series The Leftovers (2014–2017). Qualley then appeared in the dark comedy The Nice Guys (2016) and in Netflix's supernatural thriller Death Note (2017).\n\nQualley gained acclaim and a nomination for a Primetime Emmy Award for portraying actress and dancer Ann Reinking in the FX biographical drama miniseries Fosse/Verdon (2019) and the title role in the Netflix drama miniseries Maid (2021). Her biggest commercial success came with Quentin Tarantino's comedy-drama Once Upon a Time in Hollywood (2019).",
-                            gender= 1,
-                            known_for_department = "Acting",
-                            name = "Margaret Qualley",
-                            place_of_birth= "Kalispell, Montana, USA",
-                            popularity= 100.088)
-    """
--- tests/Service/test_movie_service.py
+++ tests/Service/test_movie_service.py
@@ -14,43 +14,17 @@ from tests.DAO.test_db import MockDBConnection
 
 # ------ TEST GET MOVIE BY ID -----------
 
-
 def test_get_movie_by_id_found_in_db():
     """Test method get_movie_by_id when the id is found in the database
     """
     # GIVEN
     movieservice = MovieService(DBConnector)
 
-    # Configuration du mock pour retourner un film de la base de données
-    movie = Movie(
-        id_movie=19995,
-        title="Avatar",
-        belongs_to_collection=[MovieCollection(id=87096, name="Avatar Collection")],
-        budget=237000000,
-        genres=[
-            Genre(id=28, name="Action"),
-            Genre(id=12, name="Adventure"),
-            Genre(id=14, name="Fantasy"),
-            Genre(id=878, name="Science Fiction"),
-        ],
-        origin_country=["US"],
-        original_language="en",
-        original_title="Avatar",
-        overview="In the 22nd century, a paraplegic Marine is dispatched to the moon Pandora on a unique mission, but becomes torn between following orders and protecting an alien civilization.",
-        popularity=99.959,
-        release_date=datetime.date(2009, 12, 15),
-        revenue=2923706026,
-        runtime=162,
-        vote_average=7.582,
-        vote_count=31385,
-        adult=False,
-    )
-
     # WHEN
     result = movieservice.get_movie_by_id(19995)
 
     # THEN
-    assert result.title == movie.title
+    assert result.title == "Avatar"
 
 
 def test_get_movie_by_id_found_in_tmdb():
@@ -60,33 +34,10 @@ def test_get_movie_by_id_found_in_tmdb():
     # Verifie que ce nest pas dans la DAO
     assert movieservice.movie_dao.get_by_id(19995) == None
     
-    movie = Movie(
-        id_movie=19995,
-        title="Avatar",
-        belongs_to_collection=[MovieCollection(id=87096, name="Avatar Collection")],
-        budget=237000000,
-        genres=[
-            Genre(id=28, name="Action"),
-            Genre(id=12, name="Adventure"),
-            Genre(id=14, name="Fantasy"),
-            Genre(id=878, name="Science Fiction"),
-        ],
-        origin_country=["US"],
-        original_language="en",
-        original_title="Avatar",
-        overview="In the 22nd century, a paraplegic Marine is dispatched to the moon Pandora on a unique mission, but becomes torn between following orders and protecting an alien civilization.",
-        popularity=99.959,
-        release_date=datetime.date(2009, 12, 15),
-        revenue=2923706026,
-        runtime=162,
-        vote_average=7.582,
-        vote_count=31385,
-        adult=False,
-    )
     # WHEN
     result = movieservice.get_movie_by_id(19995)
     # THEN
-    assert result.title == movie.title
+    assert result.title == "Avatar"
 
 
 def test_get_movie_by_id_not_found():
@@ -113,36 +64,11 @@ def test_get_movie_by_title_in_db():
     # GIVEN
     movieservice = MovieService(DBConnector)
 
-    # Configuration du mock pour retourner un film de la base de données
-    movie = Movie(
-        id_movie=19995,
-        title="Avatar",
-        belongs_to_collection=[MovieCollection(id=87096, name="Avatar Collection")],
-        budget=237000000,
-        genres=[
-            Genre(id=28, name="Action"),
-            Genre(id=12, name="Adventure"),
-            Genre(id=14, name="Fantasy"),
-            Genre(id=878, name="Science Fiction"),
-        ],
-        origin_country=["US"],
-        original_language="en",
-        original_title="Avatar",
-        overview="In the 22nd century, a paraplegic Marine is dispatched to the moon Pandora on a unique mission, but becomes torn between following orders and protecting an alien civilization.",
-        popularity=99.959,
-        release_date=datetime.date(2009, 12, 15),
-        revenue=2923706026,
-        runtime=162,
-        vote_average=7.582,
-        vote_count=31385,
-        adult=False,
-    )
-
     # WHEN
     result = movieservice.get_movie_by_title("Avatar")[0]
 
     # THEN
-    assert result.id_movie == movie.id_movie
+    assert result.id_movie == 19995
 
 def test_get_movie_by_title_in_tmdb():
     # GIVEN
@@ -151,34 +77,11 @@ def test_get_movie_by_title_in_tmdb():
     # Verifie que ce nest pas dans la DAO
     assert movieservice.movie_dao.get_by_title("Avatar") == None
     
-    movie = Movie(
-        id_movie=19995,
-        title="Avatar",
-        belongs_to_collection=[MovieCollection(id=87096, name="Avatar Collection")],
-        budget=237000000,
-        genres=[
-            Genre(id=28, name="Action"),
-            Genre(id=12, name="Adventure"),
-            Genre(id=14, name="Fantasy"),
-            Genre(id=878, name="Science Fiction"),
-        ],
-        origin_country=["US"],
-        original_language="en",
-        original_title="Avatar",
-        overview="In the 22nd century, a paraplegic Marine is dispatched to the moon Pandora on a unique mission, but becomes torn between following orders and protecting an alien civilization.",
-        popularity=99.959,
-        release_date=datetime.date(2009, 12, 15),
-        revenue=2923706026,
-        runtime=162,
-        vote_average=7.582,
-        vote_count=31385,
-        adult=False,
-    )
     # WHEN
     result = movieservice.get_movie_by_title("Avatar")[0]
 
     # THEN
-    assert result.id_movie == movie.id_movie
+    assert result.id_movie == 19995
 
 
 def test_get_movie_by_title_not_found():
@@ -204,7 +107,10 @@ def test_create_movies():
     # WHEN / THEN
     list_movies = movieservice.create_movies(movie_data)[0]
     
-    movie = Movie(
+    assert list_movies.title == "Avatar"
+
+"""
+movie = Movie(
         id_movie=19995,
         title="Avatar",
         belongs_to_collection=[MovieCollection(id=87096, name="Avatar Collection")],
@@ -227,6 +133,4 @@ def test_create_movies():
         vote_count=31385,
         adult=False,
     )
-
-    assert list_movies.title == "Avatar"
-
+    """
